670f6d7f26fb239d5f91d941ea30805908308dab
InvictusEd <qqdas522@gmail.com>
2023-07-18 22:04:59 +0800

    first commit

---
+++ src/main/java/com/museum/museumofworldstatues/MuseumOfWorldStatuesApplication.java
@@ -0,0 +1,13 @@
+package com.museum.museumofworldstatues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumOfWorldStatuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumOfWorldStatuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/museum/museumofworldstatues/MuseumOfWorldStatuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.museum.museumofworldstatues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumOfWorldStatuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
