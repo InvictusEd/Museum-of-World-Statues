d7137036c45a4ffd7ae48d321433b8419eb77b6b
PengZeL <2047741972@11.com>
2023-07-26 23:18:10 +0800

    7/26 23:18 完成用户忘记密码模块搭建及注册界面注册时间的修改

--- src/main/java/com/museum/museumofworldstatues/entity/User.java
+++ src/main/java/com/museum/museumofworldstatues/entity/User.java
@@ -6,13 +6,13 @@ import java.util.Date;
 
 @Data
 public class User {
-    private Long id;
-    private String userName;
-    private String password;
-    private String nickName;
-    private String email;
-    private String birthday;
-    private Integer isAdmin;
-    private String imgUrl;
-    private Date createTime;
+    private Long id;//id
+    private String userName;//用户名
+    private String password;//密码
+    private String nickName;//昵称
+    private String email;//邮箱地址
+    private String birthday;//生日
+    private Integer isAdmin;//是否为管理员 0 1
+    private String imgUrl;//头像
+    private Date createTime;//创建时间
 }
